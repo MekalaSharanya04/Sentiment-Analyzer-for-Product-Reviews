a3c2a2a8a530b5bfca74fa6c356477e54f7b6eaa
Sharanya Mekala <mekalasharanya06@gmail.com>
2026-08-03 23:02:47 +0530

    Create train_model.py for ML model training
    This module outlines the training process for various ML algorithms.

---
+++ src/src/train_model.py
@@ -0,0 +1,10 @@
+"""
+train_model.py
+
+This module will train the Machine Learning model using the processed dataset.
+
+Algorithms to be implemented:
+- Logistic Regression
+- Naive Bayes
+- Support Vector Machine (SVM)
+"""
