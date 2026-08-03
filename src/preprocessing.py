fae405bbd04ac1bb1bb8a825bfa41f7068adce06
Sharanya Mekala <mekalasharanya06@gmail.com>
2026-08-03 23:01:22 +0530

    Create preprocessing.py for text preprocessing tasks
    Add preprocessing module for product review text.

---
+++ src/preprocessing.py
@@ -0,0 +1,13 @@
+"""
+preprocessing.py
+
+This module is responsible for preprocessing product review text.
+
+Future Tasks:
+- Remove punctuation
+- Convert text to lowercase
+- Remove stop words
+- Tokenization
+- Lemmatization
+- TF-IDF Vectorization
+"""
